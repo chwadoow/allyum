b743dfd93d0f349157ace276939189c08ac5b295
moses ngige <moses32ngigz@gmail.com>
2022-07-27 21:47:20 +0300

    added more div tag in SecondData.js for styling

--- src/Pages/SecondData.js
+++ src/Pages/SecondData.js
@@ -2,15 +2,22 @@ import React from 'react'
 
 function SecondData({name,ingredients,link,youtube,steps}) {
   return (
-    <div className='display'>
-       <img src={link} alt={name} style={{width:'100%'}} />
-    <h1>{name}</h1>
-    {ingredients}
-    {steps}
-   
-
+    <div class="flip-card">
+  <div class="flip-card-inner">
+    <div class="flip-card-front">
+    <img src={link} alt={name} style={{width:'300px;height:300px'}} />
     </div>
+    <div class="flip-card-back">
+      <h1>{name}</h1>
+      <p>{ingredients}</p>
+      <p>{steps}</p>
+    </div>
+  </div>
+</div>
+  
   )
 }
 
 export default SecondData
+
+
